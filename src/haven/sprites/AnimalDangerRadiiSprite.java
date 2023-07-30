5db4fa1a47cba69353da5d8b0b8cce71f539bf6f
Nightdawg <unqdawg@gmail.com>
2023-07-30 17:14:47 +0300

    Hotfix: Animal danger radii clickable fix

--- src/haven/sprites/AnimalDangerRadiiSprite.java
+++ src/haven/sprites/AnimalDangerRadiiSprite.java
@@ -13,7 +13,7 @@ import java.nio.ShortBuffer;
 // @FromResource(name = "gfx/fx/bprad", version = 8)
 // ND: If you move the class, it no longer overwrites loftar's resource
 public class AnimalDangerRadiiSprite extends Sprite {
-	static final Pipe.Op blacc = Pipe.Op.compose(new BaseColor(new Color(0, 0, 0, 140)), new States.LineWidth(4));
+	static final Pipe.Op blacc = Pipe.Op.compose(new BaseColor(new Color(0, 0, 0, 140)), new States.LineWidth(4), Clickable.No);
     VertexBuf.VertexData posa;
     VertexBuf vbuf;
     Model smod, emod;
