c43fd5081c1ca4a92f2a773968dd13769c7ec7e3
Nightdawg <unqdawg@gmail.com>
2023-09-05 18:00:46 +0300

    Change tooltip for Wear to Durability

--- src/haven/res/ui/tt/wear/Wear.java
+++ src/haven/res/ui/tt/wear/Wear.java
@@ -8,11 +8,13 @@ import java.awt.image.BufferedImage;
 @haven.FromResource(name = "ui/tt/wear", version = 4)
 public class Wear extends ItemInfo.Tip {
     public final int d, m;
+    public final double percentage;
 
     public Wear(Owner owner, int d, int m) {
 	super(owner);
 	this.d = d;
 	this.m = m;
+    this.percentage = Math.ceil(((m-d)/(m/100d)));
     }
 
     public static ItemInfo mkinfo(Owner owner, Object... args) {
@@ -21,7 +23,9 @@ public class Wear extends ItemInfo.Tip {
 
     public BufferedImage tipimg() {
 	if(d >= m)
-	    return(RichText.render(String.format("Wear: $col[255,128,128]{%,d/%,d}", d, m), 0).img);
-	return(RichText.render(String.format("Wear: %,d/%,d", d, m)).img);
+        return(RichText.render(String.format("Durability: $col[255,128,128]{%,d/%,d} $col[255,255,255]{(%,.1f%%)}", (m-d), m, percentage), 0).img); // ND: Remember to add the width to RichText render, in order for the colors to work
+    return(RichText.render((String.format("Durability: %,d/%,d (%,.2f%%)", (m-d), m, percentage))).img);
+//        return(RichText.render(String.format("Durability: $col[255,128,128]{%,d/%,d}", m-d, m), 0).img);
+//	return(RichText.render(String.format("Durability: %,d/%,d", m-d, m)).img);
     }
 }
