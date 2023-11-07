8494e176c3e4cffc4eaeaf5dd3ac4c78d463d8a8
Meeku <failsound124@gmail.com>
2023-11-08 00:32:58 +0100

    v1.0.16

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -40,7 +40,7 @@ import java.io.*;
 import java.nio.file.*;
 
 public class Config {
-	public static final String clientVersion = "v1.0.15"; //
+	public static final String clientVersion = "v1.0.16"; //
 	public static String webClientVersion = "";
 	public static final File HOMEDIR = new File("").getAbsoluteFile();
     public static final Properties jarprops = getjarprops();
